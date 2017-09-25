cd4b29b797271df1a2fd7701dc39bc6913684dd5
Igor <igor@amperka.ru>
2017-09-25 12:51:09 +0300

    return copyright

--- TVout.cpp
+++ TVout.cpp
@@ -1,3 +1,31 @@
+/*
+Copyright (c) 2010 Myles Metzer
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+*/
+/*
+This library provides a simple method for outputting data to a tv
+from a frame buffer stored in sram.  This implementation is done
+completly by interupt and will return give as much cpu time to the
+application as possible.
+*/
+
 #include "TVout.h"
 
 /* call this to start video output with the default resolution.
